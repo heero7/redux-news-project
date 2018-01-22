fb66ec1d8e5c3b593ccac9a13e008f7687a85a04
Kevin Mudiandambo <Kevin.Mudiandambo@nisc.coop>
2018-01-22 16:32:33 -0600

    Added Header and  Footer components and setup Home container

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+// in charge of bringing all routes
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+// COMPONENTS
+import Header from './Header';
+import Footer from './Footer';
+
+// CONTAINERS
+import Home from '../containers/Home';
 
 class App extends Component {
   render() {
     return (
-     <div>Hello, World</div>
+      <BrowserRouter>
+        <div>
+          <Header/>
+          <Switch>
+            <Route exact path="/" component={Home}/>
+          </Switch>
+          <Footer />
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div>
+                Copyright DN 2018
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
