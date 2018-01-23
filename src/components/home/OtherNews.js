17e8211a231d566762557c74716ccd9ffc2d5712
heero7 <kevin7.mudia@gmail.com>
2018-01-23 12:30:52 -0600

    Added components of other news

--- src/components/home/OtherNews.js
+++ src/components/home/OtherNews.js
@@ -1,11 +1,42 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+
+const renderOtherNews = ({otherNews}) => {
+    if (otherNews) {
+        return otherNews.map((newsItem) => {
+            return (
+                <Link to={`/news/${newsItem.id}`} key={newsItem.id} className="item">
+                    <div className="left"
+                        style={{background: `url(/images/articles/${newsItem.img})`}}></div>
+                    <div className="right">
+                        <h3>{newsItem.title}</h3>
+                        <div className="category-tag">{newsItem.category}</div>
+                        <div className="tags">
+                            <span>
+                                <i className="fa fa-eye"></i>
+                                {newsItem.views}
+                            </span>
+                            <span>
+                                <i className="fa fa-thumbs-up"></i>
+                                {newsItem.likes[0]}
+                            </span>
+                        </div>
+                    </div>
+                </Link>
+            );
+        });
+    }
+}
+
 const OtherNews = (props) => {
     console.log(props);
     return (
-        <div>
-            OtherNews
+        <div className="other-news">
+            <h2>Other News</h2>
+            <div className="other-news-items">
+                {renderOtherNews(props)}
+            </div>
         </div>
     )
 }
