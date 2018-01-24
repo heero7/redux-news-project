c9bed6417219943a656eba5b01790d8192b5c726
heero7 <kevin7.mudia@gmail.com>
2018-01-24 11:26:52 -0600

    App is styled and completed

--- src/containers/GalleryItem.js
+++ src/containers/GalleryItem.js
@@ -1,73 +1,82 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 import { selectedGallery, clearSelectedGallery } from "../actions";
 import { bindActionCreators } from "redux";
 
 import Slider from "react-slick";
 
+import Counter from "./LikesCounter";
+
 const settings = {
-    dots: true,
-    infinite: true,
-    arrows: true,
-    speed: 500
-}
+  dots: true,
+  infinite: true,
+  arrows: true,
+  speed: 500
+};
 
 class GalleryItem extends Component {
+  componentDidMount() {
+    this.props.selectedGallery(this.props.match.params.id);
+  }
 
-    componentDidMount(){
-        this.props.selectedGallery(this.props.match.params.id);
-    }
+  componentWillUnmount() {
+    this.props.clearSelectedGallery();
+  }
 
-    componentWillUnmount(){
-        this.props.clearSelectedGallery();
-    }
-
-    renderSlider = ({selected}) => {
-        if (selected) {
-            const gallery = selected[0];
-            return (
-                <div>
-                    <h3>The best of {gallery.artist}</h3>
-                    <Slider {...settings}>
-                        {gallery.images.map((item, index) => {
-                            return (
-                                <div key={index} className="slide-item">
-                                    <div>
-                                        <div className="image"
-                                            style={{background:`url(/images/galleries/${item.img})`}}>
-                                        </div>
-                                        <div className="description">
-                                            <span>{item.desc}</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            )
-                        })}
-                    </Slider>
+  renderSlider = ({ selected }) => {
+    if (selected) {
+      const gallery = selected[0];
+      return (
+        <div>
+          <h3>The best of {gallery.artist}</h3>
+          <Slider {...settings}>
+            {gallery.images.map((item, index) => {
+              return (
+                <div key={index} className="slide-item">
+                  <div>
+                    <div
+                      className="image"
+                      style={{
+                        background: `url(/images/galleries/${item.img})`
+                      }}
+                    />
+                    <div className="description">
+                      <span>{item.desc}</span>
+                    </div>
+                  </div>
                 </div>
-            );
-        }
+              );
+            })}
+          </Slider>
+          <Counter
+            articleId={gallery.id}
+            likes={gallery.likes[0]}
+            dislikes={gallery.likes[1]}
+            section="galleries"
+            type="HANDLE_LIKES_GALLERY"
+          />
+        </div>
+      );
     }
+  };
 
-    render() {
-        const item = this.props.galleries;
-        return (
-            <div className="slide-item-wrap">
-                {this.renderSlider(item)}
-            </div>
-        );
-    }
+  render() {
+    const item = this.props.galleries;
+    return <div className="slide-item-wrap">{this.renderSlider(item)}</div>;
+  }
 }
 
 function mapStateToProps(state) {
-    console.log(state);
-    return {
-        galleries: state.galleries
-    }
+  return {
+    galleries: state.galleries
+  };
 }
 
 function mapDispatchToProps(dispatch) {
-    return bindActionCreators({ selectedGallery, clearSelectedGallery}, dispatch)
+  return bindActionCreators(
+    { selectedGallery, clearSelectedGallery },
+    dispatch
+  );
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(GalleryItem);
